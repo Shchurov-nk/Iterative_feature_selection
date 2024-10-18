d168c68b7a98e9fa7b33ab7fed0ef95397ce286b
Nickolay Shchurov <shchurov_no2@mail.ru>
2024-10-18 15:46:57 +0300

    bugfix relative path

--- code/streamlit_app.py
+++ code/streamlit_app.py
@@ -2,12 +2,12 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv("data/parsed/described.csv", index_col=0)
+df = pd.read_csv("../data/parsed/described.csv", index_col=0)
 
 one_line = df.T.reset_index()
 one_line["index"] = one_line["index"].astype(float)
 
-masks = pd.read_csv("data/masks/95_00_masks.csv", index_col=0)
+masks = pd.read_csv("../data/masks/95_00_masks.csv", index_col=0)
 mask = masks["Cu"]
 masked = one_line[mask.values!=0]
 
